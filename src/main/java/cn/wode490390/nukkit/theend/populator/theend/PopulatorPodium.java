7e77838b94dd74c3d1f1ac13a8c965d1541662fe
wode490390 <wode490390@qq.com>
2019-06-19 15:55:42 +0800

    1.0.1

--- src/main/java/cn/wode490390/nukkit/theend/populator/theend/PopulatorPodium.java
+++ src/main/java/cn/wode490390/nukkit/theend/populator/theend/PopulatorPodium.java
@@ -1,14 +1,26 @@
 package cn.wode490390.nukkit.theend.populator.theend;
 
+import cn.nukkit.Server;
 import cn.nukkit.level.ChunkManager;
+import cn.nukkit.level.Level;
 import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.level.format.generic.BaseFullChunk;
 import cn.nukkit.level.generator.populator.type.Populator;
 import cn.nukkit.math.NukkitRandom;
 import cn.nukkit.math.Vector3;
+import cn.nukkit.scheduler.ServerScheduler;
+import cn.wode490390.nukkit.theend.task.GenerationTask;
 
 public class PopulatorPodium extends Populator {
 
+    private Level level;
+    private boolean zero_negativeOne = false;
+    private boolean negativeOne_negativeOne = false;
+    private boolean negativeOne_zero = false;
+    private boolean generated = false;
+
     private boolean actived;
+    private int y;
 
     public PopulatorPodium() {
         this(false);
@@ -27,41 +39,79 @@ public class PopulatorPodium extends Populator {
         if (level.getBlockIdAt(0, y, 0) != END_STONE) {
             return;
         }
+        level.setBlockAt(0, y, 0, BEDROCK);
+        this.level = chunk.getProvider().getLevel();
+        this.y = y;
 
-        for (int i = -1; i <= 32; i++) {
-            for (int x = -4; x <= 4; x++) {
-                for (int z = -4; z <= 4; z++) {
-                    int dy = y + i;
-                    double distance = new Vector3(0, y).distance(new Vector3(x, dy, z));
-                    if (distance <= 3.5) {
-                        if (dy < y) {
-                            if (distance <= 2.5) {
-                                level.setBlockAt(x, dy, z, BEDROCK);
-                            } else if (dy < y) {
-                                level.setBlockAt(x, dy, z, END_STONE);
+        ServerScheduler scheduler = Server.getInstance().getScheduler();
+        BaseFullChunk zero_negativeOne = this.level.getChunk(0, -1, true);
+        if (!zero_negativeOne.isGenerated()) {
+            scheduler.scheduleAsyncTask(null, new GenerationTask(this.level, zero_negativeOne, this));
+        } else {
+            this.zero_negativeOne = true;
+        }
+        BaseFullChunk negativeOne_negativeOne = this.level.getChunk(-1, -1, true);
+        if (!negativeOne_negativeOne.isGenerated()) {
+            scheduler.scheduleAsyncTask(null, new GenerationTask(this.level, negativeOne_negativeOne, this));
+        } else {
+            this.negativeOne_negativeOne = true;
+        }
+        BaseFullChunk negativeOne_zero = this.level.getChunk(-1, 0, true);
+        if (!negativeOne_zero.isGenerated()) {
+            scheduler.scheduleAsyncTask(null, new GenerationTask(this.level, negativeOne_zero, this));
+        } else {
+            this.negativeOne_zero = true;
+        }
+        this.generate();
+    }
+
+    public void generateChunkCallback(int chunkX, int chunkZ) {
+        if (chunkX == 0 && chunkZ == -1) {
+            this.zero_negativeOne = true;
+        } else if (chunkX == -1 && chunkZ == -1) {
+            this.negativeOne_negativeOne = true;
+        } else if (chunkX == -1 && chunkZ == 0) {
+            this.negativeOne_zero = true;
+        }
+        this.generate();
+    }
+
+    private synchronized void generate() {
+        if (this.zero_negativeOne && this.negativeOne_negativeOne && this.negativeOne_zero && !this.generated) {
+            this.generated = true;
+            for (int i = -1; i <= 32; i++) {
+                for (int x = -4; x <= 4; x++) {
+                    for (int z = -4; z <= 4; z++) {
+                        int dy = this.y + i;
+                        double distance = new Vector3(0, this.y).distance(new Vector3(x, dy, z));
+                        if (distance <= 3.5) {
+                            if (dy < this.y) {
+                                if (distance <= 2.5) {
+                                    this.level.setBlockAt(x, dy, z, BEDROCK);
+                                } else if (dy < this.y) {
+                                    this.level.setBlockAt(x, dy, z, END_STONE);
+                                }
+                            } else if (dy > this.y) {
+                                this.level.setBlockAt(x, dy, z, AIR);
+                            } else if (distance > 2.5) {
+                                this.level.setBlockAt(x, dy, z, BEDROCK);
+                            } else if (this.actived) {
+                                this.level.setBlockAt(x, dy, z, END_PORTAL);
+                            } else {
+                                this.level.setBlockAt(x, dy, z, AIR);
                             }
-                        } else if (dy > y) {
-                            level.setBlockAt(x, dy, z, AIR);
-                        } else if (distance > 2.5) {
-                            level.setBlockAt(x, dy, z, BEDROCK);
-                        } else if (this.actived) {
-                            level.setBlockAt(x, dy, z, END_PORTAL);
-                        } else {
-                            level.setBlockAt(x, dy, z, AIR);
                         }
                     }
                 }
             }
+            for (int i = 0; i < 4; ++i) {
+                this.level.setBlockAt(0, this.y + i, 0, BEDROCK);
+            }
+            int torch = this.y + 2;
+            this.level.setBlockAt(1, torch, 0, TORCH, 1);
+            this.level.setBlockAt(-1, torch, 0, TORCH, 2);
+            this.level.setBlockAt(0, torch, 1, TORCH, 3);
+            this.level.setBlockAt(0, torch, -1, TORCH, 4);
         }
-
-        for (int i = 0; i < 4; ++i) {
-            level.setBlockAt(0, y + i, 0, BEDROCK);
-        }
-
-        int torch = y + 2;
-        level.setBlockAt(1, torch, 0, TORCH, 1);
-        level.setBlockAt(-1, torch, 0, TORCH, 2);
-        level.setBlockAt(0, torch, 1, TORCH, 3);
-        level.setBlockAt(0, torch, -1, TORCH, 4);
     }
 }
---
+++ src/main/java/cn/wode490390/nukkit/theend/task/GenerationTask.java
@@ -0,0 +1,65 @@
+package cn.wode490390.nukkit.theend.task;
+
+import cn.nukkit.Server;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.format.generic.BaseFullChunk;
+import cn.nukkit.level.generator.Generator;
+import cn.nukkit.level.generator.SimpleChunkManager;
+import cn.nukkit.scheduler.AsyncTask;
+import cn.wode490390.nukkit.theend.populator.theend.PopulatorPodium;
+
+public class GenerationTask extends AsyncTask {
+
+    public boolean state = true;
+
+    private final Level level;
+    private BaseFullChunk chunk;
+
+    private PopulatorPodium podium;
+
+    public GenerationTask(Level level, BaseFullChunk chunk, PopulatorPodium podium) {
+        this.chunk = chunk;
+        this.level = level;
+        this.podium = podium;
+    }
+
+    @Override
+    public void onRun() {
+        this.state = false;
+        Generator generator = this.level.getGenerator();
+        if (generator != null) {
+            SimpleChunkManager manager = (SimpleChunkManager) generator.getChunkManager();
+            if (manager != null) {
+                manager.cleanChunks(this.level.getSeed());
+                synchronized (manager) {
+                    try {
+                        BaseFullChunk chunk = this.chunk;
+                        if (chunk != null) {
+                            synchronized (chunk) {
+                                if (!chunk.isGenerated()) {
+                                    manager.setChunk(chunk.getX(), chunk.getZ(), chunk);
+                                    generator.generateChunk(chunk.getX(), chunk.getZ());
+                                    chunk = manager.getChunk(chunk.getX(), chunk.getZ());
+                                    chunk.setGenerated();
+                                }
+                            }
+                            this.chunk = chunk;
+                            this.state = true;
+                        }
+                    } finally {
+                        manager.cleanChunks(this.level.getSeed());
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onCompletion(Server server) {
+        if (this.state && this.level != null) {
+            if (this.chunk != null) {
+                this.podium.generateChunkCallback(this.chunk.getX(), this.chunk.getZ());
+            }
+        }
+    }
+}
